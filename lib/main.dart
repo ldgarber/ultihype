abae2e1f413e3cfd9c555e195c6fb8d48e7d0195
Leah Garber <leah.garber88@gmail.com>
2019-03-04 14:29:24 -0500

    basic onboarding slides complete

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,93 @@ import 'package:flutter/material.dart';
 import 'package:flutter_twitter_login/flutter_twitter_login.dart'; 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
+import 'package:intro_views_flutter/Models/page_view_model.dart';
+import 'package:intro_views_flutter/intro_views_flutter.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
+  // Intro Views pages: 
+  final introPages = [ 
+    PageViewModel(
+        pageColor: const Color(0xFF03A9F4),
+        bubble: Image.asset('assets/stats.png'),
+        body: Text(
+          "Keep track of your team's goals, assists, and layout D's",
+        ),
+        title: Text(
+          'TAKE STATS',
+        ),
+        textStyle: TextStyle(fontFamily: "FugazOne", color: Colors.white),
+        mainImage: Image.asset(
+          'assets/stats.png',
+          height: 285.0,
+          width: 285.0,
+          alignment: Alignment.center,
+        )),
+    PageViewModel(
+      pageColor: const Color(0xFFFF8a65),
+      iconImageAssetPath: 'assets/twitter-icon.png',
+      body: Text(
+        'Live tweet with the click of a button',
+      ),
+      title: Text('TWEET'),
+      mainImage: Image.asset(
+        'assets/twitter-icon.png',
+        height: 285.0,
+        width: 285.0,
+        alignment: Alignment.center,
+      ),
+      textStyle: TextStyle(fontFamily: 'FugazOne', color: Colors.white),
+    ),
+    PageViewModel(
+      pageColor: const Color(0xFFFFb74d),
+      iconImageAssetPath: 'assets/hype.png',
+      body: Text(
+        'Never sacrifice hype for accurate stats again',
+      ),
+      title: Text('HYPE'),
+      mainImage: Image.asset(
+        'assets/hype.png',
+        height: 285.0,
+        width: 285.0,
+        alignment: Alignment.center,
+      ),
+      textStyle: TextStyle(fontFamily: 'FugazOne', color: Colors.white),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'UltiHype',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.lightBlue,
       ),
-      home: MyHomePage(title: 'UltiHype Home Page'),
-    );
+      home: Builder( 
+        builder: (context) => 
+          IntroViewsFlutter(
+            introPages, 
+            onTapDoneButton: () {
+              Navigator.push(
+                context, 
+                MaterialPageRoute(
+                  builder: (context) => MyHomePage(title: 'UltiHype Home Page'), 
+                ),  
+              ); 
+            }, 
+            fullTransition: 100.0, 
+            doneButtonPersist: true, 
+            pageButtonTextStyles:  TextStyle(
+              fontFamily: 'FugazOne', 
+              color:  Colors.white, 
+              fontSize: 18.0, 
+            ), 
+          ), //IntroViewsFlutter
+      ), //Builder
+    ); //Material App
   }
 }
 
