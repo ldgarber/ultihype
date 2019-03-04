e8697ec9f738c2e6a485f7b677a6c2db4f049e0c
Leah Garber <leah.garber88@gmail.com>
2019-03-04 16:52:49 -0500

    working on adding homepage page

---
+++ lib/pages/home.page.dart
@@ -0,0 +1,67 @@
+import '../main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_twitter_login/flutter_twitter_login.dart'; 
+import 'package:firebase_auth/firebase_auth.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  FirebaseAuth _auth = FirebaseAuth.instance; 
+  bool isLogged = true; 
+
+  FirebaseUser myUser; 
+  String user_name; 
+  String image_url; 
+
+  void _signOut() async {
+    _auth.signOut().then((response) {
+      isLogged = false; 
+      setState(() {}); 
+    });  
+  } 
+
+  @override
+  initState() {
+    super.initState(); 
+    _auth.onAuthStateChanged
+        .firstWhere((user) => user != null) 
+        .then((user) {
+          user_name = user.displayName; 
+          image_url = user.photoUrl; 
+        }); 
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true, 
+      ),
+      body: Center(
+        child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                 "${myUser}"
+                 "${user_name}"
+                ), 
+                Image.network(image_url), 
+                FlatButton(
+                  child: const Text('Sign out'),
+                  onPressed: () async {
+                    await _signOut();
+                  },
+                )
+              ],
+            )
+      ),
+    );
+  }
+}
